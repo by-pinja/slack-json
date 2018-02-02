5aa0244ccb3ce0fd786906f13b055d4162acca17
Savpek <savolainen.pekka@gmail.com>
2018-02-02 13:24:17 +0200

    Fixed error at dummy implementation.

--- Controllers/WebHookController.cs
+++ Controllers/WebHookController.cs
@@ -1,16 +1,17 @@
 using System;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace Slack.Integration.Controllers
 {
     public class WebHookController: Controller
     {
         [HttpPost("v1/api/github")]
-        public IActionResult IncomingHook([FromBody] string content)
+        public IActionResult IncomingHook([FromBody] JObject content)
         {
             var tempFile = System.IO.Path.GetTempFileName();
-            System.IO.File.WriteAllText(content, tempFile);
+            System.IO.File.WriteAllText(tempFile, content.ToString());
             Console.WriteLine(tempFile);
             return Ok();
         }
