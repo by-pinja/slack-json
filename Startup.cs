ab1eff78846a944b1fa813192228219bc5d8e640
Savpek <savolainen.pekka@gmail.com>
2018-02-02 14:41:44 +0200

    Added swagger.

--- Startup.cs
+++ Startup.cs
@@ -1,31 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Slack.Integration
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSwaggerGen(c =>
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Slack integration",
+                        Version = "v1",
+                        Description = File.ReadAllText(Path.Combine(basePath, "README.md"))
+                    });
+            });
+
             services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Slack.Integration");
+                c.RoutePrefix = "doc";
+            });
         }
     }
 }
